5640b8cd3506c4333f634f7cd7622a1c7fb48f81
Raghav G. Jha <rgjha@users.noreply.github.com>
2021-10-22 03:22:00 -0400

    Update run.sh

--- short_ver/run.sh
+++ short_ver/run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-
+rm ../.DS_Store 
 rm .DS_Store
+rm ../full_ver/.DS_Store
 rm figs/.DS_Store
+rm figs/2MM/.DS_Store
 rm codes/.DS_Store 
 pdflatex notes1.tex
 bibtex notes1.bib
 bibtex notes1.bib
-bibtex notes1.bib
-pdflatex notes1.tex
-bibtex notes1.bib
-pdflatex notes1.tex
 pdflatex notes1.tex
 rm *.toc *.aux  *.log *.eps *Notes.bib *.blg *.out 
 clear
