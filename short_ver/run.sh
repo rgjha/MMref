6f84dbda0b1a284ea9bc053e08b598ec42583465
Raghav G. Jha <rgjha@users.noreply.github.com>
2021-10-30 19:46:52 -0400

    Update

--- short_ver/run.sh
+++ short_ver/run.sh
@@ -6,10 +6,10 @@ rm ../full_ver/.DS_Store
 rm figs/.DS_Store
 rm figs/2MM/.DS_Store
 rm codes/.DS_Store 
-pdflatex notes1.tex
-bibtex notes1.bib
-bibtex notes1.bib
-pdflatex notes1.tex
+pdflatex v1.tex
+bibtex v1.bib
+bibtex v1.bib
+pdflatex v1.tex
 rm *.toc *.aux  *.log *.eps *Notes.bib *.blg *.out 
 clear
-open notes1.pdf
+open v1.pdf
